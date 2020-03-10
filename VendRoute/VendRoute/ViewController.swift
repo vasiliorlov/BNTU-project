ff6e3929a2abb55d0d22befdb3e3c7cd8a093fb2
Vasili Orlov <vasilij_orlov@stylesoft.by>
2020-03-10 11:01:21 +0300

    first init commit

---
+++ VendRoute/VendRoute/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendRoute
+//
+//  Created by Vasili Orlov on 3/10/20.
+//  Copyright © 2020 Vasili Orlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
